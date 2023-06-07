a6938987ec72b1784cde46e99e34fa175c4bf43c
burcinismail8 <burcinismail8@gmail.com>
2023-06-07 22:47:13 +0300

    Fronend project setup

---
+++ lama-frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
